3c1596fc6785da04ca2845cb08f2258aa3235916
DraScript <203324047+INDRA969696@users.noreply.github.com>
2026-09-16 19:15:42 +0700

    Create info.js

---
+++ public/info.js
@@ -0,0 +1,3 @@
+var homePageText = `Welcome to the DoxiGame! Explore our collection of games and enjoy your time here.`;
+const paragrafHome = document.querySelector(".home-page-paragraph");
+paragrafHome.textContent = homePageText;
