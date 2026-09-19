39237a332c731b946036d6343b4ddd6ea4616466
DraScript <203324047+INDRA969696@users.noreply.github.com>
2026-09-19 10:56:55 +0000

    Update info.js

--- public/JS/info.js
+++ public/JS/info.js
@@ -2,6 +2,7 @@ var homePageText = `Welcome to the DoxiGame! Explore our collection of games and
 var creditsPageText = `Developered by Indra\n Design by Indra\n Hosted by vercel (Free Host)`
 
 const paragrafHome = document.querySelector(".home-page-paragraph");
-
+const paragrafCredits = document.querySelector(".credits-page-paragraph");
 
 paragrafHome.textContent = homePageText;
+paragrafCredits.textContent = creditsPageText;
