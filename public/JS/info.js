fb60c51e7920430d2af4a88676c54f6045714495
DraScript <203324047+INDRA969696@users.noreply.github.com>
2026-09-19 11:01:30 +0000

    Fix formatting of creditsPageText string

--- public/JS/info.js
+++ public/JS/info.js
@@ -1,5 +1,7 @@
 var homePageText = `Welcome to the DoxiGame! Explore our collection of games and enjoy your time here.`;
-var creditsPageText = `Developered by Indra\n Design by Indra\n Hosted by vercel (Free Host)`
+var creditsPageText = `Developered by Indra
+Design by Indra
+Hosted by vercel (Free Host)`
 
 const paragrafHome = document.querySelector(".home-page-paragraph");
 const paragrafCredits = document.querySelector(".credits-page-paragraph");
